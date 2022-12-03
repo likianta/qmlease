314fd763f804cee8c11845459bcce07e7e3e893c
likianta <likianta@foxmail.com>
2022-12-03 20:50:03 +0800

    Refactor model

--- qmlease/qmlside/model.py
+++ qmlease/qmlside/model.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from qtpy.QtCore import QAbstractListModel
 from qtpy.QtCore import QModelIndex
 
@@ -5,14 +7,16 @@ from ..qtcore import slot
 
 
 class T:  # 'TypeHint'
-    from typing import Any, Dict, Iterable, List
-    
-    Item = Dict[str, Any]
-    Items = List[Item]
-    
-    RoleNames = Iterable[str]
-    Role2Name = Dict[int, str]
-    Name2Role = Dict[str, int]
+    Defaults = t.Dict[str, t.Any]
+    Item = t.Dict[str, t.Any]
+    Items = t.List[Item]
+    Role2Name = t.Dict[int, str]
+    Name2Role = t.Dict[str, int]
+    RoleNames = t.Union[
+        t.Dict[str, t.Any],  # suggested
+        t.Tuple[str, ...],
+        t.List[str]
+    ]
 
 
 class Model(QAbstractListModel):
@@ -23,15 +27,21 @@ class Model(QAbstractListModel):
         https://stackoverflow.com/questions/54687953/declaring-a
             -qabstractlistmodel-as-a-property-in-pyside2
     """
-    _role_2_name: T.Role2Name
-    _name_2_role: T.Name2Role
+    _defaults: T.Defaults  # see also `self._auto_complete`
     _items: T.Items
+    _name_2_role: T.Name2Role
+    _role_2_name: T.Role2Name
     
     def __init__(self, role_names: T.RoleNames):
         super().__init__(None)
         self._name_2_role = {x: i for i, x in enumerate(role_names)}
-        self._role_2_name = {v: k for k, v in self._name_2_role.items()}
+        self._role_2_name = {i: x for i, x in enumerate(role_names)}
         self._items = []
+        if isinstance(role_names, dict):
+            self._defaults = role_names
+        else:
+            self._defaults = {x: None for x in role_names}
+            #   TODO: or `{x: '', ...}`?
     
     @property
     def role_names(self):
@@ -51,80 +61,143 @@ class Model(QAbstractListModel):
         return self._items[index]
     
     # -------------------------------------------------------------------------
-    # pyside api
+    # api
     # tip: all params which named `item` or `items` accept partial dict.
     
-    def append(self, item: T.Item):
+    @slot(dict)
+    def append(self, item: T.Item) -> None:
         self.beginInsertRows(
             QModelIndex(), self.rowCount(), self.rowCount()
         )
-        self._items.append(self._fill_item(item))
+        self._items.append(self._auto_complete(item))
         self.endInsertRows()
     
-    def append_many(self, items: T.Items):
+    @slot(list)
+    def append_many(self, items: T.Items) -> None:
         self.beginInsertRows(
             QModelIndex(), self.rowCount(), self.rowCount() + len(items) - 1
         )
-        self._items.extend(map(self._fill_item, items))
+        self._items.extend(map(self._auto_complete, items))
         self.endInsertRows()
     
-    def insert(self, index: int, item: T.Item):
+    @slot(int, dict)
+    def insert(self, index: int, item: T.Item) -> None:
         self.beginInsertRows(
             QModelIndex(), index, index
         )
-        self._items.insert(index, self._fill_item(item))
+        self._items.insert(index, self._auto_complete(item))
         self.endInsertRows()
     
-    def insert_many(self, index: int, items: T.Items):
+    @slot(int, list)
+    def insert_many(self, index: int, items: T.Items) -> None:
         self.beginInsertRows(
             QModelIndex(), index, index + len(items) - 1
         )
-        self._items[index:index] = list(map(self._fill_item, items))
+        self._items[index:index] = list(map(self._auto_complete, items))
         self.endInsertRows()
     
-    def pop(self):
+    @slot(result=dict)
+    def pop(self) -> T.Item:
         self.beginRemoveRows(
             QModelIndex(), len(self._items) - 1, len(self._items) - 1
         )
-        self._items.pop(0)
+        out = self._items.pop(0)
         self.endRemoveRows()
+        return out
     
-    def pop_many(self, count: int):
+    @slot(int, result=list)
+    def pop_many(self, count: int) -> T.Items:
         assert count > 0
         self.beginRemoveRows(
             QModelIndex(), len(self._items) - count, len(self._items) - 1
         )
-        self._items = self._items[:-count]
+        a, b = self._items[:-count], self._items[-count:]
+        self._items = a
         self.endRemoveRows()
+        return b
     
-    def delete(self, index: int):
+    @slot(int, result=dict)
+    def delete(self, index: int) -> T.Item:
         self.beginRemoveRows(
             QModelIndex(), index, index
         )
-        self._items.pop(index)
+        out = self._items.pop(index)
         self.endRemoveRows()
+        return out
     
-    def delete_many(self, index: int, count: int):
+    @slot(int, int, result=list)
+    def delete_many(self, index: int, count: int) -> T.Items:
         assert count > 0
         self.beginRemoveRows(
             QModelIndex(), index, index + count - 1
         )
-        self._items = self._items[:index] + self._items[index + count:]
+        a, b = (self._items[:index] + self._items[index + count:],
+                self._items[index:index + count])
+        self._items = a
         self.endRemoveRows()
+        return b
     
-    def clear(self):
+    @slot(int, int)
+    def move(self, old_index: int, new_index: int) -> None:
+        self.beginMoveRows(
+            QModelIndex(), old_index, old_index, QModelIndex(), new_index
+        )
+        item = self._items.pop(old_index)
+        self._items.insert(new_index, item)
+        self.endMoveRows()
+    
+    @slot(int, int, int)
+    def move_many(self, old_index: int, new_index: int, count: int) -> None:
+        assert count > 0
+        self.beginMoveRows(
+            QModelIndex(), old_index, old_index + count - 1,
+            QModelIndex(), new_index
+        )
+        items = self._items[old_index:old_index + count]
+        self._items[old_index:old_index + count] = []
+        self._items[new_index:new_index] = items
+        self.endMoveRows()
+    
+    @slot(int, result=bool)
+    def move_up(self, index: int) -> bool:
+        if index > 0:
+            self.move(index, index - 1)
+            return True
+        return False
+    
+    @slot(int, result=bool)
+    def move_down(self, index: int) -> bool:
+        if index < len(self._items) - 1:
+            self.move(index, index + 1)
+            return True
+        return False
+    
+    @slot()
+    def clear(self) -> None:
         self.beginRemoveRows(
             QModelIndex(), 0, len(self._items) - 1
         )
         self._items.clear()
         self.endRemoveRows()
     
+    @slot(int, result=dict)
     def get(self, index: int) -> T.Item:
         return self._items[index]
     
-    def get_many(self, start: int, end: int) -> T.Items:
+    @slot(int, result=list)
+    @slot(int, int, result=list)
+    def get_many(
+            self,
+            start: int = None,
+            end: int = None,  # TODO: shall we use `count` instead?
+    ) -> T.Items:
+        if start is not None and end is None:
+            start, end = 0, start
+        else:
+            assert start is not None and end is not None
         return self._items[start:end]
     
+    @slot(int, dict)
     def update(self, index: int, item: dict) -> None:
         self._items[index].update(item)
         # emit signal of `self.dataChanged` to notify qml side that some item
@@ -138,38 +211,21 @@ class Model(QAbstractListModel):
             qindex, qindex, [self._name_2_role[x] for x in item.keys()]
         )
     
-    def update_many(self, start: int, end: int, items: T.Items) -> None:
-        assert len(items) == end - start >= 0
-        if end == start: return
+    @slot(int, list)
+    def update_many(self, start: int, items: T.Items) -> None:
+        if not items: return
+        end = start + len(items)
         for old, new in zip(self._items[start:end], items):
             old.update(new)
         qindex_start = self.createIndex(start, 0)
         qindex_end = self.createIndex(end - 1, 0)
         self.dataChanged.emit(qindex_start, qindex_end)  # noqa
     
-    set = update
-    set_many = update_many
-    
-    def _fill_item(self, item: dict) -> T.Item:
-        for k in self._name_2_role:
+    def _auto_complete(self, item: dict) -> T.Item:
+        for k, v in self._defaults.items():
             if k not in item:
-                item[k] = None  # FIXME: alternate: `item[k] = ''`
+                item[k] = v
         return item
-    
-    # -------------------------------------------------------------------------
-    # qml side api
-    
-    @slot(dict)
-    def qappend(self, item: dict):
-        self.append(item)
-    
-    @slot(int, result=dict)
-    def qget(self, index: int):
-        return self.get(index)
-    
-    @slot(int, dict)
-    def qupdate(self, index: int, item: dict):
-        self.update(index, item)
     
     # -------------------------------------------------------------------------
     # overrides
@@ -188,6 +244,6 @@ class Model(QAbstractListModel):
     def rowCount(self, parent=QModelIndex()):  # noqa
         return len(self._items)
     
-    def roleNames(self) -> T.Dict[int, bytes]:
+    def roleNames(self) -> t.Dict[int, bytes]:
         # return self._role_2_name
         return {k: v.encode('utf-8') for k, v in self._role_2_name.items()}
