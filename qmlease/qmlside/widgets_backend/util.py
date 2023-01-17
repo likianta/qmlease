f1b308a6468dafdb379397c6847504c126a729f2
likianta <likianta@foxmail.com>
2023-01-17 17:59:03 +0800

    Simplify `util.file_dialog` calling style

--- qmlease/qmlside/widgets_backend/util.py
+++ qmlease/qmlside/widgets_backend/util.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from .__ext__ import QObject
 from .__ext__ import slot
 
@@ -17,16 +19,13 @@ class Util(QObject):
             return 'Ubuntu Mono'
     
     @slot(result=str)
-    @slot(str, result=str)
+    @slot(dict, result=str)
     @slot(str, str, result=str)
-    @slot(str, str, str, result=str)
-    @slot(str, str, str, str, result=str)
-    @slot(str, str, str, str, str, result=str)
-    @slot(str, str, str, str, str, bool, result=str)
     def file_dialog(
             self,
             action='open',
             type_='file',
+            type_filter: t.Union[dict, str, None] = None,
             start_dir='',
             title='',
             backend='qt',
@@ -36,6 +35,13 @@ class Util(QObject):
         args:
             action: literal['open', 'save']
             type_: literal['file', 'folder']
+            type_filter:
+                dict: dict[str label, list_or_tuple patterns]
+                    e.g.
+                        {'Image': ['*.png', '*.jpg']}
+                        {'Text': ['*.txt', '*.md'], 'Image': ['*.png', '*.jpg'],
+                         'All': ['*']}
+                str: see QFileDialog.
             start_dir:
                 if not specified, will do the default behavior (remembering
                 last selected path).
@@ -45,19 +51,11 @@ class Util(QObject):
             use_native_dialog: bool. deprecated!
                 set to False may resolve some issue that cannot remember the
                 last opened directory (it's a rare case).
-        
-        tip:
-            params are too many. you can use `pyside.kwcall` to bypass the slot
-            limitation:
-                // example.qml
-                const path = pyside.kwcall(
-                    lkutil, 'file_dialog', {
-                        'action'   : 'open',
-                        'type_'    : 'folder',
-                        'start_dir': '.',
-                    }
-                )
         """
+        if isinstance(action, dict):  # this is a workaround for qml call.
+            kwargs = action
+            return self.file_dialog(**kwargs)
+        
         if not title:
             title = '{do} a {thing}'.format(
                 do='Open' if action == 'open' else 'Save',
@@ -72,10 +70,34 @@ class Util(QObject):
                 print(':v3', 'invalid start direcotry, will fallback to '
                              'default behavior', start_dir)
                 start_dir = ''
+        # make sure type_filter finally be a string or null stuff.
+        if type_filter:
+            if isinstance(type_filter, dict):
+                if backend == 'qt':
+                    type_filter = ';;'.join(
+                        '{label} ({patterns})'.format(
+                            label=label,
+                            patterns=' '.join(patterns)
+                        ) for label, patterns in type_filter.items()
+                    )
+                else:
+                    type_filter = ' '.join(
+                        ' '.join(patterns) for patterns in type_filter.values()
+                    )
+        else:
+            if backend == 'qt':
+                type_filter = ...
+            else:
+                type_filter = None
         
         if backend == 'qt':
             from qtpy.QtWidgets import QFileDialog
-            kwargs = {'parent': None, 'caption': title, 'dir': start_dir}
+            kwargs = {
+                'parent' : None,
+                'caption': title,
+                'dir'    : start_dir,
+                'filter' : type_filter,
+            }
             if not use_native_dialog:
                 # noinspection PyUnresolvedReferences
                 kwargs['options'] = QFileDialog.DontUseNativeDialog
@@ -88,13 +110,17 @@ class Util(QObject):
                 if type_ == 'file':
                     return QFileDialog.getSaveFileName(**kwargs)[0]
                 else:
-                    raise ValueError('Cannot save folder.')
+                    raise ValueError('Cannot save folder!')
         else:
             from tkinter import Tk
             from tkinter import filedialog
             root = Tk()
             root.withdraw()
-            kwargs = {'title': title, 'initialdir': start_dir or None}
+            kwargs = {
+                'title'     : title,
+                'initialdir': start_dir or None,
+                'filetypes' : type_filter,
+            }
             if action == 'open':
                 if type_ == 'file':
                     return filedialog.askopenfilename(**kwargs)
@@ -104,7 +130,7 @@ class Util(QObject):
                 if type_ == 'file':
                     return filedialog.asksaveasfilename(**kwargs)
                 else:
-                    raise ValueError('Cannot save folder.')
+                    raise ValueError('Cannot save folder!')
 
 
 util = Util()
