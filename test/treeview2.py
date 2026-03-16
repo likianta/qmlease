13061c84b9cff21dbb0bf354e914db4a89e60098
Likianta <likianta@foxmail.com>
2026-03-16 23:30:38 +0800

    Create tree model by Python

---
+++ test/treeview2.py
@@ -0,0 +1,43 @@
+import qmlease as qe
+from lk_utils import fs
+
+class TreeView(qe.QObject):
+    @qe.Slot(object)
+    def init_ui(self, window):
+        @qe.bind_signal(window.pathSubmit)
+        def _(path: str):
+            print(path, ':v2')
+            assert fs.isdir(path)
+
+            def recurse(folder):
+                children = []
+                for d in fs.find_dirs(folder):
+                    children.append({
+                        'type': 'folder',
+                        'name': d.name,
+                        'path': d.path,
+                        'checked': False,
+                        'children': recurse(d.path),
+                    })
+                for f in fs.find_files(folder):
+                    children.append({
+                        'type': 'file',
+                        'name': f.name,
+                        'path': f.path,
+                        'checked': False,
+                    })
+                return children
+
+            model = [
+                {
+                    'type': 'folder',
+                    'name': fs.basename(path),
+                    'path': fs.normpath(path),
+                    'checked': False,
+                    'children': recurse(path),
+                }
+            ]
+            window['model'] = model
+
+qe.app.register(TreeView(), 'main')
+qe.app.run(fs.xpath('treeview2.qml'))
---
+++ test/treeview2.qml
@@ -0,0 +1,33 @@
+import QtQuick
+import QtQuick.Layouts
+import QmlEase
+import QmlEase.Composites
+
+Window {
+    id: root
+
+    property var model
+    signal pathSubmit(string path)
+
+    ColumnLayout {
+        anchors {
+            fill: parent
+            margins: 24
+        }
+        TextInput {
+            Layout.fillWidth: true
+            label: 'Input directory'
+            onEditingFinished: (path) => root.pathSubmit(path)
+        }
+        TreeView {
+            Layout.fillWidth: true
+            Layout.fillHeight: true
+            checkable: true
+            model: root.model
+        }
+    }
+
+    Component.onCompleted: {
+        py.main.init_ui(this)
+    }
+}
